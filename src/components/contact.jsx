b4141aac863b0c8e9995574d3a145603c65ca5db
The Ace <udonsichisom02@gmail.com>
2023-06-04 15:47:54 +0100

    feat: added a contact section (desktop)

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -12,7 +12,74 @@ function Contact({ title, icon, handleOpenMenu }) {
         <span>{title}</span>
       </p>
 
-      {/* content: Ready to embark on this adventure together? Let's make some frontend magic! */}
+      <h2 className="contact__heading">
+        Ready to embark on this adventure together? Let&apos;s make some{" "}
+        <span>frontend magic!</span>
+      </h2>
+
+      <form action="" className="contact__form">
+        <div>
+          <label htmlFor="name">
+            Full Name <span className="important">*</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your full name"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email Address <span className="important">*</span>
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your email address"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="tel">
+            Phone Number <span className="optional">(optional)</span>
+          </label>
+          <input
+            type="tel"
+            name="tel"
+            id="tel"
+            placeholder="Your phone number"
+          />
+        </div>
+        <div>
+          <label htmlFor="budget">
+            Budget <span className="optional">(optional)</span>
+          </label>
+          <input
+            type="text"
+            name="budget"
+            id="budget"
+            inputMode="number"
+            placeholder="Your budget in dollars"
+          />
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message <span className="important">*</span>
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            placeholder="Write your message here..."
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
     </section>
   );
 }
