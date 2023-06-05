48b69e6a310a95e75fe9a7d7cef0cf275b841163
The Ace <udonsichisom02@gmail.com>
2023-06-05 21:20:24 +0100

    refactor: made menu icon bar fully accessible to screen readers

--- src/components/iconBar.jsx
+++ src/components/iconBar.jsx
@@ -8,32 +8,52 @@ import { FiMail } from "react-icons/fi";
 
 function IconBar({ currentSection }) {
   return (
-    <aside className="iconBar" aria-label="menu icon bar">
-      <a href="#home" aria-label="go to home" data-title="Home">
-        <HiOutlineHome className={currentSection === 0 ? "current" : ""} />
-      </a>
-      <a href="#about" aria-label="go to about" data-title="About">
-        <CgProfile className={currentSection === 1 ? "current" : ""} />
-      </a>
-      <a href="#skills" aria-label="go to skills" data-title="Skills">
-        <GiSmallFire className={currentSection === 2 ? "current" : ""} />
-      </a>
-      <a href="#portfolio" aria-label="go to portfolio" data-title="Portfolio">
-        <BsLightningCharge className={currentSection === 3 ? "current" : ""} />
-      </a>
-      <a
-        href="#testimonial"
-        aria-label="go to testimonial"
-        data-title="Testimonial"
-      >
-        <BiMessageSquareDetail
-          className={currentSection === 4 ? "current" : ""}
-        />
-      </a>
-      <a href="#contact" aria-label="go to contact" data-title="Contact">
-        <FiMail className={currentSection === 5 ? "current" : ""} />
-      </a>
-    </aside>
+    <nav className="iconBar" aria-label="menu icon bar">
+      <ul role="list" aria-label="secondary navigation">
+        <li>
+          <a href="#home" aria-label="go to home" data-title="Home">
+            <HiOutlineHome className={currentSection === 0 ? "current" : ""} />
+          </a>
+        </li>
+        <li>
+          <a href="#about" aria-label="go to about" data-title="About">
+            <CgProfile className={currentSection === 1 ? "current" : ""} />
+          </a>
+        </li>
+        <li>
+          <a href="#skills" aria-label="go to skills" data-title="Skills">
+            <GiSmallFire className={currentSection === 2 ? "current" : ""} />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#portfolio"
+            aria-label="go to portfolio"
+            data-title="Portfolio"
+          >
+            <BsLightningCharge
+              className={currentSection === 3 ? "current" : ""}
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="#testimonial"
+            aria-label="go to testimonial"
+            data-title="Testimonial"
+          >
+            <BiMessageSquareDetail
+              className={currentSection === 4 ? "current" : ""}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="#contact" aria-label="go to contact" data-title="Contact">
+            <FiMail className={currentSection === 5 ? "current" : ""} />
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
