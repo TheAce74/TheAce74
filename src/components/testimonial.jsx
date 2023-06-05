1ff422491da11e996fad1d7e28848b34f67c3691
The Ace <udonsichisom02@gmail.com>
2023-06-05 21:36:40 +0100

    chore: added aria labels for testimonial handles for accessibility reasons

--- src/components/testimonial.jsx
+++ src/components/testimonial.jsx
@@ -25,30 +25,35 @@ function Testimonial({ title, icon, handleOpenMenu }) {
       name: "Victor Iloenyenwa",
       handle: "@ProCode47",
       icon: () => <BsGithub />,
+      platform: "GitHub",
       body: '"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, maxime dolore? Quae est commodi incidunt cumque quo exercitationem nihil rerum ullam ex laudantium, officia, eaque quisquam quibusdam natus libero voluptate!"',
     },
     {
       name: "Christopher Asor",
       handle: "@asor_christopher",
       icon: () => <BsInstagram />,
+      platform: "Instagram",
       body: "\"Chisom is a very unique and talented frontend developer. He combines effective and efficient implementation of functionalities with great attention to detail. He's a great team player and always tries to keep up good communication with his client, thanks to his great communication skill. He has shown a great sense of responsibility in the few projects he's worked on with me and I'll recommend him to every hiring manager because he'll be an excellent addition and improvement to any team!\"",
     },
     {
       name: "Jeffrey Adjari",
       handle: "@Walter-Tronics",
       icon: () => <BsGithub />,
+      platform: "GitHub",
       body: '"I have worked with Chisom Udonsi for years and he has always been a professional. He can work under pressure and easily adapt to change, embraces diversity and is open to growth opportunities. He\'s a great problem-solver with great leadership skills. He can easily work with anyone because of his interpersonal talent. I hope he grows and continues to thrive in his endeavours."',
     },
     {
       name: "Henry Kanu",
       handle: "@Stan545",
       icon: () => <BsGithub />,
+      platform: "GitHub",
       body: '"Chisom, also known as the "Senku Ishigami of the Dev World", is a tech enthusiast and highly skilled frontend developer. His relentless pursuit of knowledge and unwavering determination have made a profound impact on my programming journey. Chisom\'s mastery of frontend development is awe-inspiring, as he effortlessly translates complex concepts into elegant code. With his guidance and expertise, I have experienced tremendous growth and gained invaluable insights. Chisom\'s exceptional mentorship inspires positive change, making him a source of inspiration in my life."',
     },
     {
       name: "Tochukwu Nwankwo",
       handle: "@Emmanuetoks",
       icon: () => <BsGithub />,
+      platform: "GitHub",
       body: '"Chisom is a very remarkable programmer and mentor. His quick problem-solving skills and structured lifestyle make him one of the best programmers in the tech niche of our school. His efficient teaching skills and flawless communication enabled me to grasp whatever he taught me or any other person in just minutes. Of course, all these qualities of his are just to name a few; working and interacting with him will reveal just how good he is at what he does."',
     },
   ];
@@ -74,7 +79,9 @@ function Testimonial({ title, icon, handleOpenMenu }) {
             <div>
               <div>
                 <h3>{testimonial.name}</h3>
-                <p>{testimonial.handle}</p>
+                <p aria-label={`${testimonial.platform} handle`}>
+                  {testimonial.handle}
+                </p>
               </div>
               {testimonial.icon()}
             </div>
